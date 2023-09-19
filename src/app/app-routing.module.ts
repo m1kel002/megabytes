64fbb41545e24af5dbc4683c83007a5985572a87
Michael Florendo <michael.florendo.002@gmail.com>
2023-09-19 22:10:22 +0800

    added home page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './home/containers/welcome-page/welcome-page.component';
+import { HomePageComponent } from './home/containers/home/home-page.component';
 
 const routes: Routes = [{
   path: '',
   component: WelcomePageComponent
-}];
+},
+{
+  path: 'home',
+  component: HomePageComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/containers/home/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  standalone: true
+
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
