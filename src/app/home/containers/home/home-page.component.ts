8606e34ca69fdbd51f155e6cf9ee75d2e2f029d6
Michael Florendo <michael.florendo.002@gmail.com>
2023-09-23 16:04:15 +0800

    implemented sidenav menu

--- src/app/home/containers/home/home-page.component.ts
+++ src/app/home/containers/home/home-page.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatIconModule } from '@angular/material';
+import { BehaviorSubject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [
+    MatIconModule,
+    CommonModule
+  ]
 
 })
 export class HomePageComponent implements OnInit {
 
+  showMenu = new BehaviorSubject<boolean>(false);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMenu.next(!this.showMenu.value)
   }
 
 }
