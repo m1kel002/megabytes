96ea1fbd991d521aefd899f0edd581a3553ff703
Michael Florendo <michael.florendo.002@gmail.com>
2023-10-01 18:11:49 +0800

    init implementation for signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './account/containers/welcome-page/welcome-page.component';
 import { HomePageComponent } from './home/containers/home/home-page.component';
+import { SignupComponent } from './account/containers/signup/signup.component';
 
-const routes: Routes = [{
-  path: '',
-  component: WelcomePageComponent
-},
-{
-  path: 'home',
-  component: HomePageComponent
-}
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePageComponent,
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
 ];
 
 @NgModule({
