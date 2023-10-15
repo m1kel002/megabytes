fe63022158cf805e81f56fd19502fa939f8d896f
Michael Florendo <michael.florendo.002@gmail.com>
2023-10-15 17:08:35 +0800

    WIP signup page; Fixed angular material style not being applied properly

--- src/app/account/components/signup/signup.component.ts
+++ src/app/account/components/signup/signup.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
 })
 export class SignupComponent implements OnInit {
   constructor() {}
