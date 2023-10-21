46e0f2e1e60222280e2bf62e28934870b52a7a18
Michael Florendo <michael.florendo.002@gmail.com>
2023-10-21 10:19:46 +0800

    WIP for signup form drawer in welcome page

--- src/app/account/containers/welcome-page/welcome-page.component.ts
+++ src/app/account/containers/welcome-page/welcome-page.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-// import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
@@ -9,7 +8,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { SignupComponent } from '../../components/signup/signup.component';
 import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
-// import {}
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
@@ -23,9 +23,11 @@ import { DialogComponent } from 'src/app/shared/components/dialog/dialog.compone
     MatDialogModule,
     SignupComponent,
     DialogComponent,
+    MatSidenavModule,
   ],
 })
 export class WelcomePageComponent implements OnInit {
+  isDrawerOpened$ = new BehaviorSubject<boolean>(false);
   protected ctaForm = this.fb.group({
     email: this.fb.control('', Validators.required),
     password: this.fb.control('', Validators.required),
@@ -39,5 +41,9 @@ export class WelcomePageComponent implements OnInit {
       width: '600px',
       position: { right: '0' },
     });
+  }
+
+  toggleDrawer() {
+    this.isDrawerOpened$.next(!this.isDrawerOpened$.value);
   }
 }
