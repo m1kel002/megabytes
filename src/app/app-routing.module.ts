4efebc58f5d149f4ebdb7547052055b5a41a2ff3
Michael Florendo <michael.florendo.002@gmail.com>
2023-12-08 18:56:48 +0800

    Removed signup component in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './account/containers/welcome-page/welcome-page.component';
 import { HomePageComponent } from './home/containers/home/home-page.component';
-import { SignupDialogComponent } from './account/containers/signup-dialog/signup-dialog.component';
 
 const routes: Routes = [
   {
@@ -12,10 +11,6 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomePageComponent,
-  },
-  {
-    path: 'signup',
-    component: SignupDialogComponent,
   },
 ];
 
