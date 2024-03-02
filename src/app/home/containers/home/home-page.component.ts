91f565c9dc769cabaf2b6a0906bfb6c5ffbd42a4
Michael Florendo <michael.florendo.002@gmail.com>
2024-03-02 19:24:17 +0800

    feat(): implementation for profile overlay

--- src/app/home/containers/home/home-page.component.ts
+++ src/app/home/containers/home/home-page.component.ts
@@ -1,28 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { BehaviorSubject } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from '../../components/post/post.component';
 import { AccountService } from 'src/app/account/services/account.service';
 import { AccountRepository } from 'src/app/shared/repositories/account.repository';
+import { ProfileDialogComponent } from '../../components/profile-dialog/profile-dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   standalone: true,
-  imports: [MatIconModule, CommonModule, PostComponent, MatIconModule],
+  imports: [
+    MatIconModule,
+    CommonModule,
+    PostComponent,
+    MatIconModule,
+    ProfileDialogComponent,
+    MatDialogModule,
+    OverlayModule,
+  ],
 })
 export class HomePageComponent implements OnInit {
+  @ViewChild('profileToggle') profileToggle: ElementRef;
   showMenu = new BehaviorSubject<boolean>(false);
   posts = [1, 2, 3, 4, 5];
+  userProfile$ = this.accountRepository.profile$;
 
-  constructor(private accountRepository: AccountRepository) {}
+  constructor(
+    private accountRepository: AccountRepository,
+    public matDialog: MatDialog,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUserProfile();
+  }
 
   toggleMenu() {
     this.showMenu.next(!this.showMenu.value);
   }
 
-  getUserProfile() {}
+  getUserProfile() {
+    this.accountRepository.profile$.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  onClickLogout() {
+    this.accountRepository.logout();
+    this.router.navigate(['/login']);
+  }
 }
